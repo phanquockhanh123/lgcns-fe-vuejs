0850b0caeda78b12d78a13f68b4250b5ee9e6968
phanquockhanh123 <khanhphanquoc68@gmail.com>
2024-06-04 00:35:58 +0700

    handle ant design

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
 <template>
   <div class="wrapper">
     <nav-bar></nav-bar>
+    <h1>This is dashboard page</h1>
   </div>
 </template>
 
@@ -17,15 +18,6 @@ export default {
 }
 </script>
 <style scoped>
-.wrapper {
-    background: url("../assets/images/719756-beautiful-background-images-1920x1200-for-tablet.jpg");
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-attachment: fixed;
-    overflow: hidden;
-    min-height: 110vh;
-    /* background: rgba(39, 39, 39, 0.4); */
-}
 </style>
 
 
--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,36 +1,134 @@
 <template>
-    <main>
-        <nav-bar></nav-bar>
-        <div>
-            <h1>List Users</h1>
-            <button class="btn btn-outline-primary"><router-link to="/users/create">Create</router-link></button>
+    <nav-bar></nav-bar>
+    <a-card title="List books" class="w-100">
+        <div class="row mb-3">
+            <div class="d-flex ml-3">
+                <a-form-item label="Title" name="title">
+                    <a-input v-model:value="title" placeholder="Title" autocomplete="off" />
+                </a-form-item>
+                <a-form-item label="Author" name="author">
+                    <a-input v-model:value="author" placeholder="Author" autocomplete="off" />
+                </a-form-item>
+                <a-button type="primary" @click.prevent="getBooksList">
+                    Search
+                </a-button>
+            </div>
+            <div class="col-12 d-flex justify-content-end">
+                <a-button type="primary">
+                    Create
+                </a-button>
+            </div>
         </div>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">First</th>
-                    <th scope="col">Last</th>
-                    <th scope="col">Handle</th>
-                </tr>
-            </thead>
-            <tbody class="table-group-divider">
-                <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                </tr>
-            </tbody>
-        </table>
-    </main>
+        <div class="row">
+            <div class="col-12 table-responsive">
+                <a-table :dataSource="listBooks" :columns="columns" class="table">
+                    <template #headerCell="{ column }">
+                    </template>
+                    <template #bodyCell="{ column, index, record }">
+                        <template v-if="column.key === 'action'">
+                            <a-space>
+                                <a-button type="primary">
+                                    <EditOutlined />
+                                </a-button>
+                                <a-button type="primary" danger>
+                                    <DeleteOutlined />
+                                </a-button>
+                            </a-space>
+                        </template>
+                    </template>
+                </a-table>
+            </div>
+        </div>
+    </a-card>
 </template>
 
 <script>
+import { EditOutlined, DeleteOutlined, PlusOutlined, SearchOutlined } from '@ant-design/icons-vue';
 import NavBar from '../components/NavBar.vue';
+import axios from 'axios';
 
 export default {
     name: 'UserView',
-    components: { NavBar }
+    components: {
+        NavBar,
+        EditOutlined,
+        DeleteOutlined,
+        PlusOutlined,
+        SearchOutlined
+    },
+    data() {
+        return {
+            title: "",
+            author: "",
+            listBooks: [],
+            columns: [
+                {
+                    title: 'ID',
+                    dataIndex: 'id',
+                    key: 'id',
+                },
+                {
+                    title: 'Title',
+                    dataIndex: 'title',
+                    key: 'title',
+                },
+                {
+                    title: 'Author',
+                    dataIndex: 'authorName',
+                    key: 'authorName',
+                },
+                {
+                    title: 'Isbn',
+                    dataIndex: 'isbn',
+                    key: 'isbn',
+                },
+                {
+                    title: 'Synopsis',
+                    dataIndex: 'synopsis',
+                    key: 'synopsis',
+                },
+                {
+                    title: 'BookCover',
+                    dataIndex: 'bookCover',
+                    key: 'bookCover',
+                }
+            ],
+        };
+    },
+    created() {
+        this.getBooksList()
+    },
+    methods: {
+        async getBooksList() {
+            this.token = localStorage.getItem("token");
+            console.log(this)
+            if (this.token != "") {
+                await axios
+                    .get("http://localhost:8081/api/v1/admin/books", {
+                        params: {
+                            author: this.author,
+                            title: this.title,
+                            page: 0,
+                            size: 5
+                        },
+                        headers: {
+                            Authorization: "Bearer " + this.token,
+                        },
+                    })
+                    .then((response) => {
+                        this.listBooks = response.data.data;
+                        // this.totalItems = response.data.totalElements;
+                        // this.currentPage = response.data.number;
+                        // this.itemsPerPage = response.data.size;
+                        console.log(response.data);
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                        this.errored = true;
+                    })
+                    .finally(() => (this.loading = false));
+            }
+        }
+    }
 }
 </script>
